f438eb59ab0652085745bb09db68603d1ccda4a3
Jonas Genannt <jonas@brachium-system.net>
2015-05-02 12:51:02 +0200

    added pypi files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sshkeymanager',
+  packages = ['sshkeymanager'],
+  version = '0.1',
+  description = 'A SSH Public Key Manager',
+  author = 'Jonas Genannt',
+  author_email = 'genannt@debian.org',
+  url = 'https://github.com/hggh/sshkeymanager-django',
+  download_url = 'https://github.com/hggh/sshkeymanager-django/tarball/0.1',
+  keywords = ['cfgmgmt', 'ssh', 'keys'],
+  classifiers = [],
+)
