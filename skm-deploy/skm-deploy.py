5511fe3b603fdc5c3a55a97c6197db623181f427
Jonas Genannt <jonas@brachium-system.net>
2015-05-04 21:29:21 +0200

    add cli programm to deploy keys

---
+++ skm-deploy/skm-deploy.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import requests
+try:
+  import ConfigParser
+except:
+  import configparser as ConfigParser
+  pass
+
+
+
+DEFAULT_CONFIG='/etc/skm-deploy.conf'
+CONFIG=None
+CONFIG_FILTER_TYPE=None
+CONFIG_FILTER_VALUE=None
+POST_DATA={}
+
+def _get_config(filename):
+  setting = ConfigParser.ConfigParser()
+  setting.read(filename)
+  cfg = {}
+  for opt in setting.options('default'):
+    cfg[opt] = setting.get('default', opt)
+  return cfg
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-t', '--filter-type', help='run filter against: [group|environment|host]')
+  parser.add_argument('-f', '--filter-value', help='filter value.')
+  parser.add_argument('-c', '--config', help='configration with secret token and URL to webservice. defaults: /etc/skm-deploy.conf')
+  args = parser.parse_args()
+
+  if args.config:
+    CONFIG=args.config
+  else:
+    CONFIG=DEFAULT_CONFIG
+
+  if os.access(CONFIG, os.R_OK) is False:
+    print('Error: Could not open configuration for reading: ' + CONFIG)
+    exit(1)
+
+  if args.filter_type:
+    if args.filter_type in [ 'group', 'host', 'environemnt' ]:
+      CONFIG_FILTER_TYPE=args.filter_type
+    else:
+      print("Error: Unkown filter type argument: " + args.filter_type)
+      exit(1)
+    if args.filter_value:
+      CONFIG_FILTER_VALUE=args.filter_value
+    else:
+      print("Error: if setting --filter-type, please add a filter-value")
+      exit(1)
+    POST_DATA['filter_type'] = CONFIG_FILTER_TYPE
+    POST_DATA['filter_value'] = CONFIG_FILTER_VALUE
+
+
+  config = _get_config(CONFIG)
+  POST_DATA['API_KEY'] = config['apikey']
+
+  r = requests.post(config['address'], POST_DATA)
+
+  if r.status_code != 200:
+    print("Error: API Returned Status Code: " + str(r.status_code))
+    exit(1)
+
+  if len(r.json()) == 0:
+    print("Warning: API returned a empty json. Please check filter and or accounts")
+    exit(1);
+
+  json = r.json()
+  for key in json:
+    host = json[key]
+    print(key)
+    print(host['environment'])
+    for account in host['accounts']:
+      print(account)
+
